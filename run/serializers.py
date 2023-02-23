d3ab5bcf01ccd88c078ab73cba612152911784cc
Alec Hannaford <alec322@gmail.com>
2023-02-22 19:29:01 -0800

    added viewset

--- run/serializers.py
+++ run/serializers.py
@@ -2,7 +2,7 @@ from .models import Run
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
-class TodoSerializer(serializers.HyperlinkedModelSerializer):
+class RunSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Run
         fields = ['id','name','category']
